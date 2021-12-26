a23b014e3001007faa5530fd7fb43ed802e946e2
petermnhull <petermnhull@gmail.com>
2021-12-26 16:33:08 +0000

    Add app address to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,6 +9,7 @@ import (
 // AppConfig stores application configuration
 type AppConfig struct {
 	Port         int           `envconfig:"APP_PORT" default:"8080"`
+	Address      string        `envconfig:"APP_ADDRESS" default:"127.0.0.1"`
 	ReadTimeout  time.Duration `envconfig:"APP_READ_TIMEOUT" default:"5s"`
 	WriteTimeout time.Duration `envconfig:"APP_WRITE_TIMEOUT" default:"5s"`
 	DatabaseURL  string        `envconfig:"DATABASE_URL"`
